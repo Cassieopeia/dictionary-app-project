79147b376301d16bf06211e64a294519fbb73cdc
Cassieopeia <76845234+Cassieopeia@users.noreply.github.com>
2021-03-22 18:22:45 +0100

    Adding dictionary app project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Dictionary</h1>
+     <h2>This will work soon..</h2>
+    </div>
+  );
+}
+
+export default App;
