d3143c9f78b8e4565c033077d1dc524103aa9135
Cassieopeia <76845234+Cassieopeia@users.noreply.github.com>
2021-04-03 10:50:08 +0200

    Update Dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -6,18 +6,19 @@ export default function Dictionary() {
     let [keyword, setKeyword] = useState("");
 
     function handleResponse(response) {
-        console.log(response.data[0]);
+        console.log(response.data[0] );
     }
     
     function search(event) {
     event.preventDefault();
-    
 
+    
      let apiUrl= `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+
      axios.get(apiUrl).then(handleResponse);
     }
 
-    // https://api.dictionaryapi.dev/api/v2/entries/en_US/hello
+  
 
     function handleKeywordChange(event) {
       setKeyword(event.target.value);  
