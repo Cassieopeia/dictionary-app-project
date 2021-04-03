20ff12a31a221f1b638f8c18537c4692dbdfd5b0
Cassieopeia <76845234+Cassieopeia@users.noreply.github.com>
2021-04-03 12:12:56 +0200

    Footer added with link

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ function App() {
      <h3>Enter a keyword you would like to search</h3>
      <Dictionary />
      </div> 
+     <div className="Footer">
+     <p> <a href="https://github.com/Cassieopeia/dictionary-app-project" target="_blank" rel="noreferrer">
+       Open-source code</a> by Cassandra Ghysel </p>
+       </div>
     </div>
   );
 }
